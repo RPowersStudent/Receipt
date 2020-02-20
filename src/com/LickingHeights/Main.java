2c26aeac44e79162d59a4a2f8e5074f4646ea2aa
Rebecca <Rebecca.powers.2021@student.lhschools.org>
2020-02-20 14:09:21 -0500

    intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,34 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("+______________________+");
+        System.out.println("|                      |");
+        System.out.println("|   EGGS GAS STATION   |");
+        System.out.println("|    123456 MAIN ST    |");
+        System.out.println("|      COLUMBUS        |");
+        System.out.println("|     OHIO 43110       |");
+        System.out.println("|                      |");
+        System.out.println("| DATE   01/15/2020    |");
+        System.out.println("| TIME   8:29AM        |");
+        System.out.println("| AUTH#  001234        |");
+        System.out.println("|         VISA         |");
+        System.out.println("|    ACCOUNT NUMBER    |");
+        System.out.println("| ####-####-####-##19  |");
+        System.out.println("|      BABY YODA       |");
+        System.out.println("|                      |");
+        System.out.println("| PUMP  PRODUCT   $/G  |");
+        System.out.println("|  04     UNLD    $2.5 |");
+        System.out.println("|                      |");
+        System.out.println("| GALLONS        TOTAL |");
+        System.out.println("| 15.35         $38.36 |");
+        System.out.println("|                      |");
+        System.out.println("|      THANK YOU!      |");
+        System.out.println("|   HAVE A NICE DAY    |");
+        System.out.println("|                      |");
+        System.out.println("|                      |");
+        System.out.println("|                      |");
+        System.out.println("+______________________+");
+    }
+}
